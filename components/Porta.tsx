869b940f50637cd3ca6a2b5aed0d7e9891f6af8e
Antonio Sabino <aslimajr@outlook.com>
2022-06-12 14:29:02 -0300

    refactor: modificando componente Porta para .tsx e incluindo os types

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,13 +1,20 @@
+import PortaModel from "../model/porta";
 import styles from "../styles/porta.module.css";
 
-export default function Porta(props) {
-  const selecionada = props.selecionada ? styles.selecionada : "";
+interface PortaPropos {
+  porta: PortaModel
+}
+
+export default function Porta(props: PortaPropos) {
+  const { porta } = props
+
+  const selecionada = porta.selecionada ? styles.selecionada : "";
 
   return (
     <div className={styles.area}>
       <div className={`${styles.estrutura} ${selecionada}`}>
         <div className={styles.porta}>
-          <div className={styles.numero}>3</div>
+          <div className={styles.numero}>{porta.numero}</div>
           <div className={styles.macaneta}></div>
         </div>
       </div>
